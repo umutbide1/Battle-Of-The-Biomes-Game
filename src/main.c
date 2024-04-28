cc9a0a35d22e7c68fe2d24ab039a698b49552287
Umut Bide <umut.bide.564@gmail.com>
2024-04-28 15:45:33 +0300

    dinamik deneme / yedekleme

--- src/main.c
+++ src/main.c
@@ -1,75 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+// Canli sınıfı
 typedef struct Canli {
-    int veri;    
-    char sembol; 
-    void (*gorunum)(struct Canli *); 
+    int veri;    // Veri
+    char sembol; // Canlinin sembolu
+    void (*gorunum)(struct Canli *); // Canlinin görünümünü yazdıracak fonksiyon işaretçisi
 } Canli;
 
-
+// Bitki sınıfı, Canli sınıfından kalıtım alır
 typedef struct {
-    Canli canli; 
+    Canli canli; // Canli sınıfı
 } Bitki;
 
-
+// Bocek sınıfı, Canli sınıfından kalıtım alır
 typedef struct {
-    Canli canli; 
+    Canli canli; // Canli sınıfı
 } Bocek;
 
-
+// Pire sınıfı, Bocek sınıfından dolaylı olarak Canli sınıfından kalıtım alır
 typedef struct {
-    Bocek bocek; 
+    Bocek bocek; // Bocek sınıfı
 } Pire;
 
-
+// Sinek sınıfı, Bocek sınıfından dolaylı olarak Canli sınıfından kalıtım alır
 typedef struct {
-    Bocek bocek; 
+    Bocek bocek; // Bocek sınıfı
 } Sinek;
 
+// Bitki nesnesi oluşturacak fonksiyon
+Bitki* bitkiOlustur(int veri);
 
-void bitkiOlustur(Bitki *);
+// Bocek nesnesi oluşturacak fonksiyon
+Bocek* bocekOlustur(int veri);
 
+// Pire nesnesi oluşturacak fonksiyon
+Pire* pireOlustur(int veri);
 
-void bocekOlustur(Bocek *);
+// Sinek nesnesi oluşturacak fonksiyon
+Sinek* sinekOlustur(int veri);
 
-
-void pireOlustur(Pire *);
-
-void sinekOlustur(Sinek *);
-
+// Canlinin görünümünü ekrana yazdıracak fonksiyon
 void gorunumYazdir(Canli *);
 
-void veriOku(const char *dosyaAdi);
+// Verileri okuyup uygun sınıf nesnelerini oluşturacak fonksiyon
+void veriOku(const char *dosyaAdi, Canli ***nesneler, int *index);
 
 
-
-void bitkiOlustur(Bitki *bitki) {
+Bitki* bitkiOlustur(int veri) {
+    Bitki *bitki = (Bitki*)malloc(sizeof(Bitki));
+    bitki->canli.veri = veri;
     bitki->canli.sembol = 'B';
     bitki->canli.gorunum = gorunumYazdir;
+    return bitki;
 }
 
-void bocekOlustur(Bocek *bocek) {
+Bocek* bocekOlustur(int veri) {
+    Bocek *bocek = (Bocek*)malloc(sizeof(Bocek));
+    bocek->canli.veri = veri;
     bocek->canli.sembol = 'C';
     bocek->canli.gorunum = gorunumYazdir;
+    return bocek;
 }
 
-void pireOlustur(Pire *pire) {
+Pire* pireOlustur(int veri) {
+    Pire *pire = (Pire*)malloc(sizeof(Pire));
+    pire->bocek.canli.veri = veri;
     pire->bocek.canli.sembol = 'P';
     pire->bocek.canli.gorunum = gorunumYazdir;
+    return pire;
 }
 
-void sinekOlustur(Sinek *sinek) {
+Sinek* sinekOlustur(int veri) {
+    Sinek *sinek = (Sinek*)malloc(sizeof(Sinek));
+    sinek->bocek.canli.veri = veri;
     sinek->bocek.canli.sembol = 'S';
     sinek->bocek.canli.gorunum = gorunumYazdir;
+    return sinek;
 }
 
 void gorunumYazdir(Canli *canli) {
-    printf("%c(%d) ", canli->sembol, canli->veri);
+    printf("%c ", canli->sembol);
 }
 
-void veriOku(const char *dosyaAdi) {
+void veriOku(const char *dosyaAdi, Canli ***nesneler, int *index) {
     FILE *dosya = fopen(dosyaAdi, "r");
     if (dosya == NULL) {
         printf("Dosya acilamadi.\n");
@@ -78,32 +92,42 @@ void veriOku(const char *dosyaAdi) {
 
     int sayi;
     while (fscanf(dosya, "%d", &sayi) != EOF) {
-        if (sayi >= 1 && sayi <= 9) {
-            Bitki bitki;
-            bitkiOlustur(&bitki);
-            bitki.canli.veri = sayi;
-            bitki.canli.gorunum(&bitki.canli);
-        } else if (sayi >= 10 && sayi <= 20) { 
-            Bocek bocek;
-            bocekOlustur(&bocek);
-            bocek.canli.veri = sayi;
-            bocek.canli.gorunum(&bocek.canli);
-        } else if (sayi >= 21 && sayi <= 50) { 
-            Sinek sinek;
-            sinekOlustur(&sinek);
-            sinek.bocek.canli.veri = sayi;
-            sinek.bocek.canli.gorunum(&sinek.bocek.canli);
+        if (sayi >= 1 && sayi <= 9) { // Bitki
+            *nesneler = (Canli**)realloc(*nesneler, (*index + 1) * sizeof(Canli*));
+            (*nesneler)[*index] = (Canli*)bitkiOlustur(sayi);
+            (*index)++;
+        } else if (sayi >= 10 && sayi <= 20) { // Bocek
+            *nesneler = (Canli**)realloc(*nesneler, (*index + 1) * sizeof(Canli*));
+            (*nesneler)[*index] = (Canli*)bocekOlustur(sayi);
+            (*index)++;
+        } else if (sayi >= 21 && sayi <= 50) { // Sinek
+            *nesneler = (Canli**)realloc(*nesneler, (*index + 1) * sizeof(Canli*));
+            (*nesneler)[*index] = (Canli*)sinekOlustur(sayi);
+            (*index)++;
         } else { // Pire
-            Pire pire;
-            pireOlustur(&pire);
-            pire.bocek.canli.veri = sayi;
-            pire.bocek.canli.gorunum(&pire.bocek.canli);
+            *nesneler = (Canli**)realloc(*nesneler, (*index + 1) * sizeof(Canli*));
+            (*nesneler)[*index] = (Canli*)pireOlustur(sayi);
+            (*index)++;
         }
     }
 
-    fclose(dosya); 
+    fclose(dosya); // Dosyayı kapat
 }
 int main() {
-    veriOku("veriler.txt");
+    Canli **nesneler = NULL;
+    int index = 0;
+    veriOku("veriler.txt", &nesneler, &index);
+
+    // Oluşturulan nesneleri ekrana yazdır
+    for (int i = 0; i < index; ++i) {
+        nesneler[i]->gorunum(nesneler[i]);
+    }
+    
+    // Oluşturulan nesnelerin belleklerini serbest bırak
+    for (int i = 0; i < index; ++i) {
+        free(nesneler[i]);
+    }
+    free(nesneler);
+    
     return 0;
 }
