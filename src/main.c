def9167e2a511158b64f58da6b064a012c22aceb
Umut Bide <umut.bide.564@gmail.com>
2024-05-01 19:15:40 +0300

    Son hal

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
 #include "herb.h"
 #include "flea.h"
 #include "habitat.h"
@@ -9,6 +11,38 @@
 #include "numberOfElements.h"
 #include "numberOfElementsInRow.h"
 
+int ilk_satirdaki_eleman_sayisi(const char *dosya_adi) {
+    FILE *dosya = fopen(dosya_adi, "r");
+    if (dosya == NULL) {
+        perror("Dosya acma hatasi");
+        return -1;
+    }
+
+    // İlk satırı oku
+    char *satir = NULL;
+    size_t satir_uzunlugu = 0;
+    ssize_t okunan = getline(&satir, &satir_uzunlugu, dosya);
+    if (okunan == -1) {
+        perror("Satir okuma hatasi");
+        fclose(dosya);
+        free(satir);
+        return -1;
+    }
+
+    // Okunan satırı elemanlara ayır ve eleman sayısını bul
+    char *eleman = strtok(satir, " \t\n");
+    int eleman_sayisi = 0;
+    while (eleman != NULL) {
+        eleman_sayisi++;
+        eleman = strtok(NULL, " \t\n");
+    }
+
+    // Bellekten ayrılan yerleri serbest bırak
+    free(satir);
+    fclose(dosya);
+
+    return eleman_sayisi;
+}
 
 
 int main() {
@@ -20,14 +54,13 @@ int main() {
     // }
     const char *dosyaAdi = "veriler.txt";
     int elemanSayisi = dosyadakiElemanSayisiniHesapla(dosyaAdi); 
-    
     char *sembollerDizisi = (char *)malloc(elemanSayisi * sizeof(char)); // Dizinin sembollerinin tutulacağı dizi
-    int *degerlerDizisi = (int *)malloc(elemanSayisi * sizeof(int)); // dizideki sembollerin karşılıkları olan degerleri tutulacağı dizi
-    
     if (sembollerDizisi == NULL) {
         printf("Semboller icin bellek ayirma hatasi!");
         return 1;
     }
+
+    int *degerlerDizisi = (int *)malloc(elemanSayisi * sizeof(int)); // dizideki sembollerin karşılıkları olan degerleri tutulacağı dizi
     if (degerlerDizisi==NULL){
         printf("Sembollere karsilik gelen degerler icin bellek ayirma hatasi");
     }
@@ -37,8 +70,6 @@ int main() {
     //     satirdakiELemenlarinSayisiDizisi[i]=sonuc.elemanSayilari[i];
     //     //printf("%d",satirdakiELemenlarinSayisiDizisi[i]);
     // }
-    
-
     
     //printf("\n");
     for (int i = 0; i < elemanSayisi; ++i) {
@@ -53,7 +84,7 @@ int main() {
         //printf("%d",degerlerDizisi[i]);
         //printf(" ");
     }
-   
+    
 
 
     for (int i = 0; i < index; ++i) {
